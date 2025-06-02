98eef4f407fa70fdf0fcb3089276748636d143f5
DiegoBustos16 <diegombustos16@gmail.com>
2025-06-02 11:19:58 -0300

    fix: filter out deleted users in email lookup

--- src/main/java/diegobustos/my_task_planner_backend/service/UserDetailsServiceImpl.java
+++ src/main/java/diegobustos/my_task_planner_backend/service/UserDetailsServiceImpl.java
@@ -16,7 +16,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) {
-        User user = userRepository.findByEmail(email)
+        User user = userRepository.findByEmailAndDeletedAtIsNull(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
         return org.springframework.security.core.userdetails.User
