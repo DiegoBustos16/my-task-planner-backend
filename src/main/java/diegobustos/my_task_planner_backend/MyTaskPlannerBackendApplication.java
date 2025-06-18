34f193b4d6bd2c0d27984547997dd2cb83e55610
DiegoBustos16 <diegombustos16@gmail.com>
2025-06-18 12:49:02 -0300

    feat: add Docker support and update application properties for JWT and CORS

--- src/main/java/diegobustos/my_task_planner_backend/MyTaskPlannerBackendApplication.java
+++ src/main/java/diegobustos/my_task_planner_backend/MyTaskPlannerBackendApplication.java
@@ -1,6 +1,5 @@
 package diegobustos.my_task_planner_backend;
 
-import diegobustos.my_task_planner_backend.config.DotenvInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyTaskPlannerBackendApplication {
 
 	public static void main(String[] args) {
-		new DotenvInitializer();
 		SpringApplication.run(MyTaskPlannerBackendApplication.class, args);
 	}
 
